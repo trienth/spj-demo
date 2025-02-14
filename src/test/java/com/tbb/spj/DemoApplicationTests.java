7192e119c83560c5cba58e143408f88e57752322
trienth <t.h.trien88@gmail.com>
2025-02-14 17:45:50 +0700

    Init project

---
+++ src/test/java/com/tbb/spj/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tbb.spj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
